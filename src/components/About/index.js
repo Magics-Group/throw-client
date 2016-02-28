a4740102b3b714eecb4f84af581bac9a0d9ed4c8
Ethan Coward <ethanjc@mac.com>
2016-02-27 22:13:24 -0800

    Something
    "I have actually done something"

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,10 +1,30 @@
 ﻿import React from 'react'
 
+import {
+    IconButton
+}
+from 'material-ui'
+
+import {
+	PropTypes
+}
+from 'react-router'
+
 export default class extends React.Component {
+
+	static contextTypes = {
+		history: PropTypes.history
+	};
+
     render() {
         return (
             <div className="col-lg-12">
-                <p>pernus</p>
+
+            	<IconButton iconClassName="material-icons" onClick={() => this.context.history.replace('/')} iconStyle={{color: '#767A7B', fontSize: '30px', top: '-2px', right: '2px'}} className="settings" >arrow_back</IconButton>
+                <h4>About</h4>
+                <p>Throw v0</p>
+                <p>creators...</p>
+                <p>licese...</p>
             </div>
         )
     }
