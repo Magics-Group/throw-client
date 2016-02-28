043d41adfee751744349294c18845b0925f306dc
Ethan Coward <ethanjc@mac.com>
2016-02-27 20:54:42 -0800

    pernus

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -4,7 +4,7 @@ export default class extends React.Component {
     render() {
         return (
             <div className="col-lg-12">
-                
+                <p>pernus</p>
             </div>
         )
     }
