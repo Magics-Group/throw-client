d711c7a5a2e921bb6ebb26b3896bc0f8044420a3
Luigi Poole <luigipoole@outlook.com>
2016-02-28 02:11:44 -0800

    refactor

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -38,7 +38,7 @@ export default class Dashboard extends React.Component {
 
         this.setState({
             loadingModal: false,
-            torrentAddOpen: false
+            urlAddOpen: false
         })
         this.props.setUrl(url)
     };
@@ -73,41 +73,17 @@ export default class Dashboard extends React.Component {
                <center>
                     <div ref="dropper">
                         <div>
-                            <div className="mainButtonHolder">
-                                 <div className="inButtonHolder">
-                                    <IconButton iconClassName="material-icons" iconStyle={{color: '#767A7B', fontSize: '30px', top: '-2px', right: '2px'}} className="settings" >settings</IconButton>
-                                    <IconButton iconClassName="material-icons" onClick={() => this.context.history.replace('/about')} iconStyle={{color: '#767A7B', fontSize: '30px', top: '-2px', right: '2px'}} className="settings" >info</IconButton>
-                                    <IconButton iconClassName="material-icons" iconStyle={{color: '#767A7B', fontSize: '30px', top: '-2px', right: '2px'}} className="torrent-dash" >view_compact</IconButton>
-                                </div>
-                            </div>
-                            <br/>
-                            <b className="fl_dd droid-bold">Drag &amp; Drop a File</b>
-                            <br/>
-                            <span className="fl_sl">or select an option below</span>
+  
+                            <span className="fl_sl">select an option from below</span>
                             <br/>
                             <br/>
+                                    <br/>
+                                            <br/>
+                                                    <br/>
                             <div className="mainButHold">
-                                <RaisedButton style={{float: 'left', width: '130px', height: '108px'}} onClick={() => this.setState({torrentAddOpen: true})} label="Add Torrent">
-                                    <img src="images/icons/torrent-icon.png" style={{marginTop: '13px'}}/>
-                                    <br/>
-                                    <span className="fl_sl lbl" style={{marginTop: '11px'}}>
-                                    Add Torrent
-                                    </span>
-                                </RaisedButton>
-                                <RaisedButton style={{width: '130px', height: '108px'}} onClick={this.addFile} label="Add Video">
-                                    <img src="images/icons/video-icon.png" style={{marginTop: '18px'}}/>
-                                    <br/>
-                                    <span className="fl_sl lbl" style={{marginTop: '15px'}}>
-                                    Add Video
-                                    </span>
-                                </RaisedButton>
-                                <RaisedButton style={{float: 'right', width: '130px', height: '108px'}} onClick={() => this.setState({urlAddOpen: true})} label="Use a URL">
-                                    <img src="images/icons/link-icon.png" style={{marginTop: '17px'}}/>
-                                    <br/>
-                                    <span className="fl_sl lbl" style={{marginTop: '10px'}}>
-                                    Use a URL
-                                    </span>
-                                </RaisedButton>
+                                <RaisedButton style={{float: 'left', width: '130px'}} onClick={() => this.setState({torrentAddOpen: true})} label="Add Torrent" />
+                                <RaisedButton style={{width: '130px'}} onClick={this.addFile} label="Add Video" />
+                                <RaisedButton style={{float: 'right', width: '130px'}} onClick={() => this.setState({urlAddOpen: true})} label="Use a URL" />
                             </div>
 
         					<Dialog
